c4289b3ad9561a82f10d5e50f541b4b0de3cf7ab
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-04 20:47:13 -0600

    updated test to check mapStateToProps

--- test/components/addCard.spec.js
+++ test/components/addCard.spec.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import { AddCard }from './../../js/components/containers/add_card';
+import { shallow, mount, render } from 'enzyme';
+import { Provider } from 'react-redux';
+import { createMockStore } from 'redux-test-utils';
+import ConnectedAddCard, { AddCard } from './../../js/components/containers/add_card';
+import { thunk, create } from './mockRedux'
 
 function setup() {
-  const props = {
+  // const props = {
+  //   selectedDeck: {
+  //     name: 'selectedDeckTestName',
+  //     cards: [1,2,3],
+  //     user: 'deckOwner',
+  //     password: 'ownerPassword'
+  //   },
+  //   user: 'deckOwner',
+  //   password: 'ownerPassword',
+  //   card: { name: 'testCard' }
+  // }
+
+  const testState = {
+    isLoggedIn: true,
     selectedDeck: {
       name: 'selectedDeckTestName',
       cards: [1,2,3],
@@ -12,25 +28,56 @@ function setup() {
     },
     user: 'deckOwner',
     password: 'ownerPassword',
-    card: { name: 'testCard' }
-  }
+  };
+  const store = createMockStore(testState);
 
-  const enzymeWrapper = mount(<AddCard {...props} />)
+  const wrapper = mount(
+    <Provider store={store}>
+      <ConnectedAddCard card={{name: 'testCard'}}/>
+    </Provider>
+  )  
+
+
 
   return {
-    props,
-    enzymeWrapper
+    testState,
+    store,
+    wrapper
   }
 }
 
 describe('AddCard container component', () => {
-  it('should render a button', () => {
-    const { enzymeWrapper } = setup();
+  const { wrapper, store } = setup();
+  const SmartComponent = wrapper.find(ConnectedAddCard);
+  const Component = wrapper.find(AddCard);
 
-    const buttonProps = enzymeWrapper.find('Button').props();
+  it('should render component with store successfully', () => {
+    expect(SmartComponent.length).toEqual(1)
+    expect(Component.length).toEqual(1)
+    expect(store.getState().isLoggedIn).toEqual(true)
+    expect(Component.prop('user')).toEqual(store.getState().user)
+    expect(Component.prop('password')).toEqual(store.getState().password)
+    expect(Component.prop('selectedDeck')).toEqual(store.getState().selectedDeck)
+    expect(Component.prop('card')).toEqual({name: 'testCard'})
+  })
+
+  it('should render a button', () => {
+    const buttonProps = Component.find('Button').props();
     expect(buttonProps.className).toEqual('cardEdit');
     expect(buttonProps.text).toEqual('add card');
     expect(buttonProps.type).toEqual('button');
-    expect(buttonProps.card).toEqual({ name: 'testCard' })
+    expect(SmartComponent.find('Button').hasClass('cardEdit')).toBe(true);
+  })
+
+  it('should handle Button clicks by dispatching updates to store', () => {
+    const selectedDeck = Component.prop('selectedDeck');
+    const sampleAction = {
+      type: 'UPDATE_DECKLIST',
+      card: Component.prop('card')
+    }
+    // button.props().onClick();
+    store.dispatch(sampleAction);
+    expect(store.getActions()).toEqual([sampleAction]);
+    expect(store.isActionDispatched(sampleAction)).toBe(true)
   })
 })
