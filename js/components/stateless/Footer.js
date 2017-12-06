7e82925a4c247c1c85a5fc9de189355cd40f7cb8
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-06 15:18:53 -0600

    moved footer into stateless components

---
+++ js/components/stateless/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let Footer = props => {
+	return (
+		<div className="footer-div">
+			<span className="footer-name">Daniel Kafer</span>
+			<a href="https://dkaf.github.io/portfolio/index.html" className="footer-link">Portfolio</a>
+		</div>
+	);
+}
+
+export default Footer;
