c9748b14d95b6938036198d97532e1858e05eea9
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-06 21:09:13 -0600

    added tests for Input component

---
+++ test/components/Input.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Input from './../../js/components/presentational/input';
+
+function setup() {
+  const props = {
+    id: 'testId',
+    type: 'testType',
+    className: 'inputClassName',
+    placeholder: 'testPlaceholder',
+    onChange: jest.fn()
+  }
+
+  const enzymeWrapper = mount(<Input {...props} />)
+  
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+describe('Input component', () => {
+  const { enzymeWrapper, props } = setup();
+  it('should render the input component', () => {
+    expect(enzymeWrapper.find('input').hasClass('inputClassName')).toBe(true)
+  })
+  it('should accept the correct props', () => {
+    expect(enzymeWrapper.prop('id')).toEqual('testId');
+    expect(enzymeWrapper.prop('type')).toEqual('testType');
+    expect(enzymeWrapper.prop('className')).toEqual('inputClassName');
+    expect(enzymeWrapper.prop('placeholder')).toEqual('testPlaceholder');
+  })
+})
