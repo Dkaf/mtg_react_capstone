5cdb8fdf00b465feb9bad44a5c3f8ec4fd90ab43
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-06 21:09:39 -0600

    Added tests for Button component

---
+++ test/components/Button.spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Button from './../../js/components/presentational/button';
+
+function setup() {
+  const props = {
+    className: 'testButton',
+    type: 'button',
+    onClick: jest.fn(),
+    text: 'button text'
+  }
+
+  const enzymeWrapper = mount(<Button {...props} />)
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+describe('Button presentational component', () => {
+  it('should render a button with a class, type, text and onClick function', () => {
+    const { enzymeWrapper } = setup();
+
+    expect(enzymeWrapper.find('button').hasClass('testButton')).toBe(true);
+
+    expect(enzymeWrapper.find('button').text()).toBe('button text')
+  })
+})
