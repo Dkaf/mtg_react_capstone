33fcfabf77d1f7557c3f166a92555b0fa5bae0b5
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-07 12:52:39 -0600

    separated DeckList into container and presentational components

---
+++ js/components/containers/DeckListContainer.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+
+import DeckList from './../stateless/DeckList';
+
+export class DeckListContainer extends Component {
+	constructor(props) {
+		super(props);
+	}
+		render() {
+			if(this.props.isLoggedIn) {
+				return (
+					<DeckList decks={this.props.decks} selectedDeck={this.props.selectedDeck}/>
+				);
+		} else {
+			return null
+		}
+	}
+};
+
+let mapStateToProps = (state, props) => {
+	return {
+		decks: state.deckList,
+		selectedDeck: state.selectedDeck.name,
+		isLoggedIn: state.isLoggedIn,
+		numberCards: state.selectedDeck.cards
+	}
+};
+
+const Container = connect(mapStateToProps)(DeckList)
+
+export default Container;
