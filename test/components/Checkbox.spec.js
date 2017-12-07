dd5e7305481fbb726ffed92372cbd9bdd28147d0
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-06 21:09:25 -0600

    Added tests for Checkbox component

---
+++ test/components/Checkbox.spec.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Checkbox from './../../js/components/presentational/checkbox';
+
+function setup() {
+  const props = {
+    id: 'testId',
+    image: 'testImageProp',
+    className: 'inputClassName',
+    value: 'testValue',
+    onClick: jest.fn()
+  }
+
+  const enzymeWrapper = mount(<Checkbox {...props} />)
+  
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+describe('Checkbox component', () => {
+  const { enzymeWrapper, props } = setup();
+
+  it('should render the correct component', () => {
+    expect(enzymeWrapper.find('div').hasClass('colorCheckbox'))
+  })
+
+  it('should accept the correct props', () => {
+    expect(enzymeWrapper.prop('id')).toEqual('testId')
+    expect(enzymeWrapper.props().image).toEqual('testImageProp')
+    expect(enzymeWrapper.props().className).toEqual('inputClassName')
+    expect(enzymeWrapper.props().value).toEqual('testValue')
+  })
+
+  it('should have a label', () => {
+    const label = enzymeWrapper.find('label');
+  })
+
+  it('should have an img', () => {
+    expect(enzymeWrapper.find('img').hasClass('colorIcon')).toBe(true)
+  })
+
+  it('should have an input', () => {
+    expect(enzymeWrapper.find('input').hasClass('inputClassName')).toBe(true)
+  })
+})
