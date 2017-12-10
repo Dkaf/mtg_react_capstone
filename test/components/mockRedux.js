21b5dc234d2542739b7dd7eccd7ca20d1760da69
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-10 14:22:41 -0600

    Mock redux for testing

---
+++ test/components/mockRedux.js
@@ -0,0 +1,30 @@
+export const thunk = ({ dispatch, getState }) => next => action => {
+  if (typeof action === 'function') {
+    return action(dispatch, getState)
+  }
+
+  return next(action)
+}
+
+export const create = () => {
+  const store = {
+    getState: jest.fn(() => ({})),
+    dispatch: jest.fn(),
+  };
+  const next = jest.fn()
+
+  const invoke = (action) => thunk(store)(next)(action)
+
+  return {store, next, invoke}
+};
+
+export const storeFake = (state) => {
+	return {
+		default: () => {},
+		subscribe: () => {},
+		dispatch: () => {},
+		getState: () => {
+			return { ...state };
+		},
+	};
+};
