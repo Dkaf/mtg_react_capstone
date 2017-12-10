1c81b349752c61f117983e220660bdec9ce27d1a
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-10 14:21:30 -0600

    fixed import path

--- test/components/Checkbox.spec.js
+++ test/components/Checkbox.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import Checkbox from './../../js/components/presentational/checkbox';
+import Checkbox from './../../js/components/stateless/Checkbox';
 
 function setup() {
   const props = {
