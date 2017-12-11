3ddbccd0da6ee676ea9d3781002911e5c07faf34
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-11 16:37:48 -0600

    Changed webpack config to handle css modules

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'js/index.js'),
+    entry: path.resolve(__dirname, 'js/'),
     output: {
         path: path.resolve(__dirname, 'build/js'),
         filename: 'index.js',
@@ -16,6 +16,10 @@ module.exports = {
           exclude: /(node_modules)/,
           loader: 'babel',
         },
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]',
+        }
       ]
     }
 };
