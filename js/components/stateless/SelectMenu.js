f3594d523e4e5ede9fb1d65eddb6c1d8d2366930
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-11 11:09:24 -0600

    added key prop to menuItems

--- js/components/stateless/SelectMenu.js
+++ js/components/stateless/SelectMenu.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const SelectMenu = (props) => {
-  const menuItems = (item) => {
-    return <option value={item}>{item}</option>
+  const menuItems = (item, i) => {
+    return <option key={i} value={item}>{item}</option>
   }
 
   return (
-    <select className={props.className} onChange={this.typeFilter}>
+    <select className={props.className} onChange={props.onChange}>
       <option>{props.name}</option>
       {props.options.map(menuItems)}
     </select>
