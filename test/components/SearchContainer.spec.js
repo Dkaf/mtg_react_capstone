80324cf903b10c124e1d867b547b48af6f0d756a
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-11 16:40:40 -0600

    Added basic tests for SearchContainer component

---
+++ test/components/SearchContainer.spec.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { Provider } from 'react-redux';
+import { createMockStore } from 'redux-test-utils';
+import SearchContainer from './../../js/components/containers/SearchContainer';
+import { thunk, create } from './mockRedux';
+
+
+function setup() {
+  const testState = {
+    isLoggedIn: true,
+    deckList: [
+      {
+        name: 'test1',
+        cards: [1,2,3]
+      },
+    ],
+    selectedDeck: {
+      name: 'testName'
+    }
+  }
+
+  const store = createMockStore(testState);
+
+  const wrapper = mount(
+    <Provider store={store}>
+      <SearchContainer />
+    </Provider>
+  )
+
+  return {
+    testState,
+    store,
+    wrapper
+  }
+}
+
+describe('SearchContainer component', () => {
+  const { store, wrapper } = setup();
+  const Component = wrapper.find(SearchContainer);
+
+  it('should render the correct component', () => {
+    expect(Component.length).toEqual(1);
+  })
+
+  it('should dispatch actions', () => {
+    const sampleAction = {
+      type: 'TEST_TYPE',
+      payload: 'test'
+    }
+    store.dispatch(sampleAction);
+
+    expect(store.getActions()).toEqual([sampleAction]);
+    expect(store.isActionDispatched(sampleAction)).toBe(true);
+  })
+
+})
