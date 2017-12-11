831625c2688e9bcf924458933fadcedab83e8280
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-11 11:08:03 -0600

    updated import paths and temporarily commented out searchResults

--- js/components/containers/SearchContainer.js
+++ js/components/containers/SearchContainer.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import store from '../store';
-import actions from '../actions/index';
+import store from '../../store';
+import { cardSearch, pageForward, pageBack} from '../../actions/card';
+import { filterReset, nameFilter, typeFilter, cmcFilter, rarityFilter, colorFilter, removeColorFilter } from '../../actions/filters';
 import { connect } from 'react-redux';
 
-import Input from './stateless/Input';
-import Checkbox from './stateless/Checkbox';
-import Button from './stateless/Button';
-import AddCardContainer from './containters/AddCardContainer';
+import Input from './../stateless/Input';
+import Checkbox from './../stateless/Checkbox';
+import Button from './../stateless/Button';
+import SelectMenu from './../stateless/SelectMenu';
+import AddCardContainer from './AddCardContainer';
 
-class Search extends React.Component {
+class SearchContainer extends React.Component {
 
 	constructor(props){
 		super(props);
@@ -19,69 +21,69 @@ class Search extends React.Component {
 	submitHandler(e) {
 		e.preventDefault();
 		console.log(store.getState());
-		store.dispatch(actions.cardSearch(store.getState().filters));
-		store.dispatch(actions.filterReset());
+		store.dispatch(cardSearch(store.getState().filters));
+		store.dispatch(filterReset());
 		e.target.reset();
 	}
 
 	pageForward() {
 		if(store.getState().page * store.getState().pageSize <= store.getState().cardSearchResults.length) {
-			store.dispatch(actions.pageForward());
+			store.dispatch(pageForward());
 		}
 	}
 
 	pageBack() {
 		if(store.getState().page != 0) {
-			store.dispatch(actions.pageBack());
+			store.dispatch(pageBack());
 		}
 	}
 
 	nameFilter(e) {
-		store.dispatch(actions.nameFilter(e.target.value));
+		store.dispatch(nameFilter(e.target.value));
 	}
 
 	cmcFilter(e) {
-		store.dispatch(actions.cmcFilter(e.target.value));
+		store.dispatch(cmcFilter(e.target.value));
 	}
 
 	typeFilter(e) {
-		store.dispatch(actions.typeFilter(e.target.value));
+		store.dispatch(typeFilter(e.target.value));
 	}
 
 	rarityFilter(e) {
-		store.dispatch(actions.rarityFilter(e.target.value));
+		store.dispatch(rarityFilter(e.target.value));
 	}
 
 	colorFilter(e) {
 		if (!e.target.checked) {
-			store.dispatch(actions.removeColorFilter(e.target.value))
+			store.dispatch(removeColorFilter(e.target.value))
 		}
 		else {
-			store.dispatch(actions.colorFilter(e.target.value));
+			store.dispatch(colorFilter(e.target.value));
 		}
 		console.log(store.getState().filters)
 	}
 
 	render() {
-		let pageContent = this.props.searchResults.slice(this.props.page * this.props.pageSize, (this.props.page * this.props.pageSize) + this.props.pageSize)
+		// let pageContent = this.props.searchResults.slice(this.props.page * this.props.pageSize, (this.props.page * this.props.pageSize) + this.props.pageSize)
 
-		let searchResults = pageContent.map( (key) => {
-			console.log(key);
-			return (
-				<li>
-					<img src={key.imageUrl}></img>
-					<AddCard card={key} />
-				</li>
-			)
-		})
+		// let searchResults = pageContent.map( (key) => {
+		// 	console.log(key);
+		// 	return (
+		// 		<li>
+		// 			<img src={key.imageUrl}></img>
+		// 			<AddCardContainer card={key} />
+		// 		</li>
+		// 	)
+		// })
 		return (
 			<div id="searchDiv">
 				<h2>Card Search</h2>
 				<form onSubmit={this.submitHandler}>
 					<Input className="cardSearch" id="cardNameInput" type="search" placeholder="Card Name" onChange={this.nameFilter} />
 					<Input className="cardSearch" id="manaCostInput" type="search" placeholder="Mana Cost" onChange={this.cmcFilter} />
-					<SelectMenu className="typeSelector" name="Card Type" options={}/>
-					<SelectMenu className="raritySelector" name="Rarity" options={}/>
+					<SelectMenu className="typeSelector" name="Card Type" onChange={this.typeFilter} options={[1,2,3]} />
+					<SelectMenu className="raritySelector" name="Rarity" onChange={this.rarityFilter} options={[4,5,6]} />
 					<label htmlFor="blackSelect" id="colorLabel">Colors</label>
 					<fieldset className="colorSelector">
 						<Checkbox className="colorOption" id="blackSelect" value="black" image="css/black_mana_button.png" onClick={this.colorFilter} />
@@ -93,7 +95,7 @@ class Search extends React.Component {
 					<Button className="submitButton" type="submit" text="Submit" />
 				</form>
 				<ul>
-					{searchResults}
+
 				</ul>
 				<i className="fa fa-arrow-circle-o-left fa-4x pageButton" id="pageBack" aria-hidden="true" onClick={this.pageBack}></i>
 				<i className="fa fa-arrow-circle-o-right fa-4x pageButton" id="pageForward" aria-hidden="true" onClick={this.pageForward}></i>
@@ -110,6 +112,6 @@ let mapStateToProps = (state, props) => {
 	}
 }
 
-let Container = connect(mapStateToProps)(Search);
+let Container = connect(mapStateToProps)(SearchContainer);
 
 export default Container;
