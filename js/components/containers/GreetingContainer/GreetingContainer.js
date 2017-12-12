14d0093aa2b41a591340bc59e5dc9ae87ac3b535
Dkaf <Daniel.j.kafer@gmail.com>
2017-12-11 19:08:05 -0600

    bound 'this' to logout

--- js/components/containers/GreetingContainer/GreetingContainer.js
+++ js/components/containers/GreetingContainer/GreetingContainer.js
@@ -11,6 +11,7 @@ import UserGreeting from './../../stateless/UserGreeting/UserGreeting';
 export class GreetingContainer extends Component{
 	constructor(props){
 		super(props);
+		this.logout = this.logout.bind(this);
 	}
 
 	logout(e) {
