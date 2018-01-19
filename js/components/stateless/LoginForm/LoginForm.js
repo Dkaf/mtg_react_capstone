4f68e0b9c1946f745dbe3d17e715bb1744b7f322
Dkaf <Daniel.j.kafer@gmail.com>
2018-01-19 16:55:50 -0600

    Changed to use semantic-ui components

--- js/components/stateless/LoginForm/LoginForm.js
+++ js/components/stateless/LoginForm/LoginForm.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Form, Container } from 'semantic-ui-react';
 import Input from './../Input/Input';
 import styles from './styles.css';
 
 const LoginForm = (props) => {
   return (
-    <div className={styles.loginForm}>
-      <form onSubmit={props.onSubmit}>
-        <Input className={styles.loginInput} placeholder="username" type="search" onChange={props.usernameChange} />
-        <Input className={styles.loginInput} placeholder="password" type="password" onChange={props.passwordChange} />
-      </form>
-      <div className={styles.login}><span onClick={props.onClick} className={styles.loginLink}>Login</span></div>
-  </div>
+    <Container>
+      <Form size="small" onSubmit={props.onSubmit}>
+        <Form.Input placeholder="username" onChange={props.usernameChange} />
+        <Form.Input placeholder="password" type="password" onChange={props.passwordChange} />
+        <Form.Button onClick={props.onClick} content="login" />
+      </Form>
+  </Container>
   );
 }
 
--- js/components/stateless/UserGreeting/UserGreeting.js
+++ js/components/stateless/UserGreeting/UserGreeting.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Header, Container } from 'semantic-ui-react';
 
 import styles from './styles.css';
 
 const UserGreeting = (props) => {
   return (
-    <div className={styles.greetingDiv}>
-      <h2 className={styles.greeting} className={styles.greetingMessage}>Welcome {props.user}!</h2>
+    <Container>
+      <Header as="h2"> Welcome {props.user}! </Header>
       <span onClick={props.logout} className={styles.logout}>Logout</span>
-    </div>
+    </Container>
   )
 }
 
